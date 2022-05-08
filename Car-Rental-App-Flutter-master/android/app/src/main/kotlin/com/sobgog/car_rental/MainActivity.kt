6494cb4db30cf8516f51e6093a48de53bcb9c93b
prashantgupta-1998 <60106216+prashantgupta-1998@users.noreply.github.com>
2022-05-08 14:15:00 +0530

    Delete Car-Rental-App-Flutter-master directory

--- Car-Rental-App-Flutter-master/android/app/src/main/kotlin/com/sobgog/car_rental/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.sobgog.car_rental
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Car-Rental-App-Flutter-master/lib/model/user_model.dart
+++
@@ -1,6 +0,0 @@
-class User {
-  final String uid;
-  final String? email;
-  final bool? emailVerifed;
-  User(this.uid, this.email, this.emailVerifed);
-}
